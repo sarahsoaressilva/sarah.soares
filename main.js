7a5525650e44c9e38454cf4dba2f8a5d3d8a6f7b
Sarah Soares <contatodev.sarah@gmail.com>
2023-04-20 17:14:27 -0300

    Cria sessão experiência

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 import './style.css'
-import javascriptLogo from '/javascript.svg'
-import viteLogo from '/vite.svg'
 import { navMobile } from './navigation.js';
 import { galeryFilter } from './filter.js';
 import { cursorAnimation } from './cursor.js';
